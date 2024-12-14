6add08a498ef35d57045c1b4e3a4edfb48d46d1a
GAUTAM CHATTERJEE <gautam.chatterjee@in.ibm.com>
2024-12-13 22:28:09 -0500

    Changing compiler plugins to Java Source 21

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,14 +1,28 @@
 package clinic.programming.training;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
     Application() {
         System.out.println("From inside application!!");
     }
 
+    private void displayList() {
+        List<String> list = new ArrayList<>();
+        list.add("USA");
+        list.add("Canada");
+        list.add("Mexico");
+        list.add("Panama");
+        list.add("Costa Rica");
+
+        list.stream().forEach(x -> System.out.println(x));
+    }
+
     public static void main(String[] args) {
         System.out.println("Starting application.");
         Application app = new Application();
-
+        app.displayList();
     }
 }
